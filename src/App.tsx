90c83600ca7cec6c173e75c66d41c8d2cf2cb144
Joachim Klug <code@joachimklug.com>
2023-10-01 03:50:50 +0200

    feat: 🎸 Generate QR Code and place it on threejs canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,159 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, MeshProps, useThree } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import QRCode from "react-qr-code";
+import * as THREE from "three";
+import { STLExporter } from "three/addons/exporters/STLExporter.js";
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const svgRef = useRef<SVGElement>();
+  const [svgImage, setSVGImage] = useState<string>(
+    '<svg><use xlink:href="sprite.svg#glasses--wine"></use></svg>'
+  );
+  const [ssid, setSsid] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  useEffect(() => {
+    if (svgRef.current) {
+      setSVGImage(svgRef.current.outerHTML);
+    }
+  }, [svgRef, ssid, setSVGImage]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Customize3d</h1>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginBottom: 2 * 8,
+          gap: 4,
+        }}
+      >
+        <label>
+          SSID:
+          <input
+            type="text"
+            value={ssid}
+            onChange={(e) => setSsid(e.target.value)}
+          />
+        </label>
+        <label>
+          PWD:
+          <input
+            type="text"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+          />
+        </label>
+      </form>
+      <div
+        style={{
+          height: "auto",
+          margin: "0 auto",
+          maxWidth: 64 * 3,
+          width: "100%",
+        }}
+      >
+        <QRCode
+          size={256}
+          ref={svgRef}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={`WIFI:T:WPA;S:${ssid};P:${pwd};;`}
+          viewBox={`0 0 256 256`}
+          level="M"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div style={{ border: "1px solid white" }}>
+        <Canvas
+          style={{ width: 800, height: 600 }}
+          camera={{ position: [0, 100, 100] }}
+        >
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+
+          {/* <Plane mesh={{ position: [0, 1, 0] }} setStl={setStl} /> */}
+          <QROverlay svg={svgImage} />
+
+          <OrbitControls />
+          <gridHelper args={[150, 15]} />
+          <axesHelper scale={10} />
+        </Canvas>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => console.log("hello")}>Click</button>
     </>
-  )
+  );
 }
 
-export default App
+interface QROverlayProps {
+  svg: string;
+}
+
+function QROverlay({ svg }: QROverlayProps) {
+  const background = /<path.*?fill="#FFFFFF"><\/path>/;
+  const newSvg = svg.replace(background, "");
+
+  // const scene = useThree((state) => state.scene);
+  const url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(newSvg);
+  const loader = new SVGLoader();
+
+  const group = new THREE.Group();
+  loader.load(url, function (data) {
+    const paths = data.paths;
+    group.scale.y *= -1;
+
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+
+      const material = new THREE.MeshBasicMaterial({
+        color: "white",
+        // color: path.color,
+        side: THREE.DoubleSide,
+        depthWrite: false,
+      });
+
+      const shapes = SVGLoader.createShapes(path);
+
+      for (let j = 0; j < shapes.length; j++) {
+        const shape = shapes[j];
+        const geometry = new THREE.ExtrudeGeometry(shape, {
+          depth: 0.1,
+          bevelEnabled: false,
+        });
+        const mesh = new THREE.Mesh(geometry, material);
+        group.add(mesh);
+      }
+    }
+
+    // scene.add(group);
+  });
+
+  return <primitive object={group}></primitive>;
+}
+
+interface PlaneProps {
+  mesh: MeshProps;
+  setStl: (stl: string) => void;
+}
+
+function Plane({ mesh, setStl }: PlaneProps) {
+  const scene = useThree((state) => state.scene);
+  const exporter = new STLExporter();
+  const stlExport = exporter.parse(scene, { binary: false });
+
+  useEffect(() => {
+    setStl(stlExport);
+  }, [stlExport, setStl]);
+
+  return (
+    <mesh {...mesh}>
+      <boxGeometry args={[100, 2, 100]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
+}
+
+export default App;
